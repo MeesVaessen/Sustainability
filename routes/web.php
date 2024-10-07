8217dfbfe5fe8c42a712ab5ea7874aa8df8b530f
mees_vaessen <mees.vaessen@beech.it>
2024-10-07 12:29:24 +0200

    Export

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,15 @@
 <?php
 
+use App\Http\Controllers\ExportController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/export/excel', [ExportController::class, 'exportExcel'])->middleware(['auth', 'verified']);
+Route::get('/export/csv', [ExportController::class, 'exportCsv'])->middleware(['auth', 'verified']);
 
 Route::get('/dashboard', function () {
     return view('dashboard');
