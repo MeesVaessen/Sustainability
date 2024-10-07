a1483b78e55d7990b81e5f2b66986ed0a0a882c0
joost_wetjens <joost.wetjens@beech.it>
2024-10-07 14:19:08 +0200

    input stuff

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -18,7 +18,7 @@ return new class extends Migration
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
-            $table->integer('distance');
+            $table->integer('distance')->nullable()->default(null);
             $table->timestamps();
         });
 
--- resources/views/components/input-bar.blade.php
+++ resources/views/components/input-bar.blade.php
@@ -4,23 +4,43 @@
             <h1 class="mb-3 text-2xl font-bold"> How did you get to work today? </h1>
         </div>
 
-        <form>
-            @csrf
-            <div class="flex flex-row">
-                <div class="flex flex-col">
-                    <label for="distance">Distance (rounded to the nearest kilometer):</label>
-                    <input class="border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 rounded-md shadow-sm" type="number" id="distance" name="distance" min="0">
-                </div>
-                <div class="flex flex-col ml-4">
-                    <label for="type">Mode of transport:</label>
+        @if(auth()->user()->distance == null)
+            <form>
+                @csrf
+                <div class="flex flex-row">
+                    <div class="flex flex-col">
+                        <label for="distance">Distance (rounded to the nearest kilometer):</label>
+                        <input class="border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 rounded-md shadow-sm" type="number" id="distance" name="distance" min="0">
+                    </div>
+                    <div class="flex flex-col ml-4">
+                        <label for="type">Mode of transport:</label>
 
-                    <select class="border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 rounded-md shadow-sm" name="type" id="type">
-                    @foreach(\App\Models\travelMode::all() as $mode)
-                            <option value={{$mode->type}}>{{$mode->type}}</option>
-                    @endforeach
-                    </select>
+                        <select class="border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 rounded-md shadow-sm" name="type" id="type">
+                            @foreach(\App\Models\travelMode::all() as $mode)
+                                <option value={{$mode->type}}>{{$mode->type}}</option>
+                            @endforeach
+                        </select>
+                    </div>
                 </div>
-            </div>
-        </form>
+            </form>
+        @else
+            <form>
+                @csrf
+                <div class="flex flex-row items-center justify-center">
+                    <div class="flex flex-col ">
+                        <label for="type">Mode of transport:</label>
+                        <select class="border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 rounded-md shadow-sm" name="type" id="type">
+                            @foreach(\App\Models\travelMode::all() as $mode)
+                                <option value={{$mode->type}}>{{$mode->type}}</option>
+                            @endforeach
+                        </select>
+                    </div>
+                </div>
+                <button></button>
+            </form>
+        @endif
+
+
+
     </div>
 </div>
